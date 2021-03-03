718d47803476637ebc008656f6a83f2b2907b721
Mollie Goforth <molliegoforth818@gmail.com>
2021-03-03 11:47:50 -0600

    todo list done

---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <h1>ToDoList</h1>
+  <h3> {{ itemsRemaining }}</h3>
+  <div v-for="item in todos" :key="item.id">
+    <todo-item :todo="item" @status-change="handleStatusChange" />
+  </div>
+  </div>
+</template>
+
+<script>
+import { todoItems } from "../data";
+import TodoItem from "./TodoItem";
+
+export default {
+ components: { TodoItem },
+ data() {
+   return{
+     todos: [...todoItems]
+   }
+ },
+methods: {
+  handleStatusChange(item) {
+    item.complete = !item.complete;
+  }
+},
+computed: {
+  itemsRemaining() {
+    return this.todos.filter((todo) => !todo.complete);
+  }
+}                            
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const todoItems = [
+    {
+        id:1,
+        name:"walk the dog",
+        complete: false
+    },
+    {
+        id:2,
+        name: "take out the trash",
+        complete: false,
+    },
+    {
+        id: 3,
+        name: "feed the kitties",
+        complete: false,
+    },
+]
